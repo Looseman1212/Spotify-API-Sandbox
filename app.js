88058f853a6bfeed04983014aacbbb1c1b0a2e65
max reid <maxwilliamreid@gmail.com>
2023-03-22 15:20:14 +1100

    First push, HTML auth form, search form, and results added, javascript API handling for OAuth and basic search functionally

---
+++ app.js
@@ -0,0 +1,170 @@
+const redirect_uri = 'http://127.0.0.1:8000/index.html' // Redirect uri
+
+let clientId = ''
+let clientSecret = ''
+
+const AUTHORIZE = 'https://accounts.spotify.com/authorize'
+const TOKEN = 'https://accounts.spotify.com/api/token'
+
+function onPageLoad() {
+  accessToken = localStorage.getItem('accessToken');
+  refreshToken = localStorage.getItem('refreshToken');
+
+  if (window.location.search.length > 0){
+    clientId = localStorage.getItem('clientId');
+    clientSecret = localStorage.getItem('clientSecret');
+    handleRedirect();
+  };
+
+  if (accessToken != null && refreshToken != null) {
+    hideAuthShowSearch();
+  };
+}
+
+function requestAuthorization() {
+  console.log('getting authorization token')
+  clientId = document.getElementById('clientId').value;
+  clientSecret = document.getElementById('clientSecret').value;
+  localStorage.setItem('clientId', clientId);
+  localStorage.setItem('clientSecret', clientSecret); // do not share your client secret with any users
+
+  let url = AUTHORIZE;
+  url += '?client_id=' + clientId;
+  url += '&response_type=code';
+  url += '&redirect_uri=' + encodeURI(redirect_uri);
+  url += '&show_dialog=true';
+  url += '&scope=user-read-playback-state playlist-read-private user-follow-read user-read-currently-playing user-read-email user-top-read user-library-read';
+  window.location.href = url; // show spotify's authoization screen
+}
+
+function handleRedirect() { // handles functions that are meant to occur after the spotify redirect
+  let authToken = parseAuthToken();
+  console.log(authToken);
+  fetchAccessToken(authToken);
+  window.history.pushState({}, document.title, "/" + "index.html"); // reverts url back to original state
+}
+
+function parseAuthToken() { // parsing URL with URL params
+  console.log("Parsing url for access/authorization token")
+  const queryString = window.location.search;
+  if (queryString.length > 0) {
+    const urlParams = new URLSearchParams(queryString);
+    let authToken = urlParams.get('code');
+    return authToken;
+  } else {
+    console.log("No access token found")
+  };
+}
+
+function fetchAccessToken(authToken) { // build body for post request
+  // create body for post request
+  let body = 'grant_type=authorization_code';
+  body += '&code=' + authToken;
+  body += '&redirect_uri=' + encodeURI(redirect_uri);
+  body += '&client_id=' + clientId;
+  body += '&client_secret=' + clientSecret;
+  requestAccessRefreshTokens(body);
+}
+
+function requestAccessRefreshTokens(body) { // creates the xmlhttprequest and hands in the body made previously
+  // send xmlhttprequest to spotify
+  let request = new XMLHttpRequest();
+  request.open('POST', TOKEN, true);
+  request.setRequestHeader('Authorization', 'Basic ' + btoa(clientId + ':' + clientSecret));
+  request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+  request.send(body);
+  request.onload = handleAccessRefreshTokenResponse; // onload points to function that will handle (and be given) the response
+}
+
+function handleAccessRefreshTokenResponse() { // .onload instance method gives over the XMLHttpRequest response to this function
+  if (this.status == 200) {
+    let data = JSON.parse(this.responseText);
+    // console.log(data)
+    let accessToken = data.access_token;
+    let refreshToken = data.refresh_token;
+    localStorage.setItem('accessToken', accessToken);
+    localStorage.setItem('refreshToken', refreshToken);
+    console.log("Access token: " + accessToken);
+    console.log("Refresh token: " + refreshToken);
+    onPageLoad();
+  } else {
+    // try to log error if it is not a 200 response
+    console.log(this.responseText);
+  }
+}
+
+function hideAuthShowSearch() { // hides the authorization form and shows the search bar when auth token is found
+  document.getElementById('auth-form').style.display = 'none';
+  document.getElementById('search-bar').style.display = 'block';
+  document.getElementById('authorised').style.display = 'block';
+}
+
+function searchSpotify() {
+  let search = document.getElementById('search').value;
+  let url = 'https://api.spotify.com/v1/search?q=' + search + '&type=' + 'artist';
+  console.log(url);
+  fetchSpotifyData(url);
+}
+
+function fetchSpotifyData(url) {
+  let request = new XMLHttpRequest();
+  request.open('GET', url, true);
+  request.setRequestHeader('Authorization', 'Bearer ' + accessToken);
+  request.send();
+  request.onload = handleSpotifySearchResponse;
+}
+
+function handleSpotifySearchResponse() {
+  if (this.status == 200) {
+    let data = JSON.parse(this.responseText);
+    let name = data.artists.items[0].name
+    let genres = data.artists.items[0].genres
+    let spotify_link = data.artists.items[0].external_urls.spotify
+    let followers = data.artists.items[0].followers.total
+    let image_url = data.artists.items[0].images[0].url
+    // console.log(`Name: ${name}, Genres: ${genres}, Spotify Link: ${spotify_link}, Followers: ${followers}, Image: ${image_url}`)
+    revealResults(name, genres, spotify_link, followers, image_url);
+  } else {
+    console.log(this.responseText);
+  }
+}
+
+function clearSearchResults() {
+  document.getElementById('artist-name').innerHTML = 'Artist Name: ';
+  document.getElementById('artist-genres').innerHTML = 'Genres: ';
+  document.getElementById('artist-follower-count').innerHTML = 'Followers: ';
+  document.getElementById('artist-spotify-link').innerHTML = 'Click to open on spotify here!';
+}
+
+function revealResults(n, g, s_link, f, i_url) {
+  clearSearchResults();
+  document.querySelector('.search-results').style.display = 'block';
+  document.getElementById('artist-image').src = i_url;
+  document.getElementById('artist-name').insertAdjacentHTML('beforeend', n);
+  document.getElementById('artist-follower-count').insertAdjacentText('beforeend', f);
+  document.getElementById('artist-spotify-link').href = s_link;
+  g.forEach(function (genre) {
+    document.getElementById('artist-genres').insertAdjacentText('beforeend', `${genre}, `);
+  });
+}
+
+
+
+
+/* URL which is built in requestAuthorization()
+https://accounts.spotify.com/en/authorize?
+client_id=3f15114ecf52461a95ef44a6b24976d7
+&response_type=code
+&redirect_uri=http://127.0.0.1:8000/index.html
+&show_dialog=true
+&scope=user-read-playback-state%20playlist-read-private%20user-follow-read%20user-read-currently-playing%20user-read-email%20user-top-read%20user-library-read */
+
+/* URL which is loaded when you are returned back to the
+http://127.0.0.1:8000/index.html?
+code=AQBlhQjOYqw4u3BEovEmGGV8TULdaI62cVzkVv88NvD2qUkHiJlowDiAyEsDAs0cyhg44_
+rRNeMCSUBT1TTGTElbfPtA7hSQPnoqTJFqT5v9w-PKfzM4mEofV-YAKHdnmblfB3LRlMtX6FKPlU9F16s6smz5rkLJOYkW0dfTTPxmHXhXAknDpBZ2sYHP-pF_B5gbxmIBVEG3QF9HNb_
+pCPFHrsALRUbNXm7wxjbkpANDI6v1GBbevgCnB66OCY2kCrCueChxVBkdRdn41H8p_
+aDZ0cjb1BLOa9NksB04U912v0oR1C1EHA_
+DGcRHnqnmf4G-rZlgOb744gV_
+x7jbDbwt0z_
+2lDF6zsx1aDBIxo3VS3A7gN3Y */
